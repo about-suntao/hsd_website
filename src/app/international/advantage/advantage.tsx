c3a74546f1819e7af2619d31be71b938597b514a
about-suntao <1550208137@qq.com>
2024-06-18 13:35:21 +0800

    fix 1

--- src/app/international/advantage/advantage.tsx
+++ src/app/international/advantage/advantage.tsx
@@ -80,7 +80,7 @@ function Advantage() {
               <h2>合作办学美高介绍</h2>
             </div>
             <div className={styles.introduce}>
-              <p><span>Foxcroft Academy</span>（福克斯克罗夫特学院）成立于1823年，摘得美国缅因州TOP 10寄宿学校的桂冠。是一所历史悠久、学术氛围浓郁的私立寄宿学校。</p>
+              <p><span>Foxcroft Academy</span>（福克斯克罗夫特学院）成立于1823年，摘得美国缅因州TOP10寄宿学校的桂冠。是一所历史悠久、学术氛围浓郁的私立寄宿学校。</p>
               <p>学校提供多达150门多元化的课程：包括21门AP课程，21门视觉和表演艺术课程，24个科技教育项目，5门外语和10门计算机科学课程等。</p>
             </div>
           </div>
