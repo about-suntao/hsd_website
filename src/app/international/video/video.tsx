d6a4fe0f1de05bce242350c2441f372de3db2d2f
about-suntao <1550208137@qq.com>
2024-07-03 11:44:03 +0800

    fix 合作办学页面修改

--- src/app/international/video/video.tsx
+++ src/app/international/video/video.tsx
@@ -4,7 +4,6 @@ import styles from './video.module.scss'
 import Image from 'next/image'
 import { Modal } from 'antd';
 
-
 import videoImg from '../../../../public/img/international/video.webp'
 
 function Video() {
@@ -28,8 +27,19 @@ function Video() {
   return (
     <div className={styles.pages}>
       <div className={styles.container}>
-        <div className={styles.btn} onClick={() => openDialog()}>
-          <div className={styles.triangular}></div>
+        <div className={styles.backImg}>
+          <Image src={videoImg} alt=''></Image>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <h2>Foxcroft Academy G-11在读中国学生访谈</h2>
+          </div>
+          <div className={styles.introduce}>
+            <p>华盛达外语学校国际部运营校长陈昊宇，来到了合作办学的美高Foxcroft Acadamy，采访了在读中国学生，倾听同学在Foxcroft Acadamy的真实经历和遇到的困难...</p>
+          </div>
+          <div className={styles.btn} onClick={() => openDialog()}>
+            <div className={styles.triangular}></div>
+          </div>
         </div>
         <div id="1" className={styles.videoDialog}>
           <Modal
