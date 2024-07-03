d4f565fe1aa1ac3b21fd6466d015ab1f7949fc7c
about-suntao <1550208137@qq.com>
2024-07-03 17:02:09 +0800

    fix 教研团队修改

--- src/app/team/management/educational/educational.tsx
+++ src/app/team/management/educational/educational.tsx
@@ -1,57 +1,90 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState, useRef } from 'react'
 import styles from './educational.module.scss'
 import Image from 'next/image';
-import fetchRequest from '@/utils/fetchRequest';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules'
+import fetchRequest from '@/utils/fetchRequest';
 import cardImg from '../../../../../public/img/team/card.webp'
 
-async function getData() {
-  const res = await fetchRequest.get('/icon/web/people/queryAll', { teamId: 3 });
-  return {
-    data: res.data,
-  }
-}
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 
-async function Educational() {
-  const { data } = await getData()
+function Educational() {
+  SwiperCore.use([Navigation]);
+  const prevBtn = useRef(null);
+  const nextBtn = useRef(null);
+  const [data, setData] = useState([])
+  const getData = async () => {
+    const res = await fetchRequest.get('/icon/web/people/queryAll', { teamId: 3 });
+    setData(res.data)
+  }
+  useEffect(() => {
+    getData()
+  }, []);
+
   return (
-    <div className={styles.pages}>
+    <section className={styles.pages}>
       <div className={styles.container}>
-        <div className={styles.title}>
-          <h2>教研团队</h2>
-          <p>Teaching & Research Team</p>
+        <div className={styles.titleBox}>
+          <div className={styles.title}>
+            <h2>教研团队</h2>
+            <p>Teaching & Research Team</p>
+          </div>
+          <div className={styles.btnGroup}>
+            <button ref={prevBtn}><LeftOutlined /></button>
+            <button ref={nextBtn}><RightOutlined /></button>
+          </div>
         </div>
-        <ul>
-          {
-            data.map((item: any) => {
-              return (
-                <li key={item.id}>
-                  <div className={styles.picture}>
-                    <Image src={item.photograph} alt={item.name} width={1000} height={1000} priority={true}></Image>
-                  </div>
-                  <div className={styles.card}>
-                    <Image src={cardImg} alt=''></Image>
-                    <h3>{item.name}</h3>
-                    <p className={styles.introduce}>{item.intro}</p>
-                    <div className={styles.honor}>
-                      {
-                        item.honors.map((i: any) => {
-                          return (
-                            <p key={i.id}>
-                              <span></span>{i.name}
-                            </p>
-                          )
-                        })
-                      }
+        <div className={styles.cardList}>
+          <Swiper
+            slidesPerView={'auto'}
+            spaceBetween={40}
+            className={styles.swiperBox}
+            onInit={(swiper: any) => {
+              // 绑定自定义按钮
+              console.log(swiper)
+              swiper.params.navigation.prevEl = prevBtn.current;
+              swiper.params.navigation.nextEl = nextBtn.current;
+              swiper.navigation.init();
+              swiper.navigation.update();
+            }}
+          >
+            {
+              data.map((item: any) => {
+                return (
+                  <SwiperSlide key={item.id}>
+                    <div className={styles.picture}>
+                      <Image src={item.photograph} alt={item.name} width={1000} height={1000} priority={true}></Image>
                     </div>
-                  </div>
-                </li>
-              )
-            })
-          }
-        </ul>
+                    <div className={styles.card}>
+                      <Image src={cardImg} alt=''></Image>
+                      <div className={styles.people}>
+                        <h3>{item.name}</h3>
+                        <p>{item.position}</p>
+                      </div>
+                      <p className={styles.introduce}>{item.intro}</p>
+                      <div className={styles.honor}>
+                        {
+                          item.honors.map((i: any) => {
+                            return (
+                              <p key={i.id}>
+                                <span></span>{i.name}
+                              </p>
+                            )
+                          })
+                        }
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                )
+              })
+            }
+          </Swiper>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
--- src/app/team/management/page.tsx
+++ src/app/team/management/page.tsx
@@ -6,12 +6,12 @@ import Educational from './educational/educational'
 
 function Management() {
   return (
-    <div>
-      <Banner title='管理团队' titleEn="Management Team"></Banner>
+    <>
+      {/* <Banner title='管理团队' titleEn="Management Team"></Banner> */}
       {/* <CoreTeam></CoreTeam> */}
       <ForeignTeachers></ForeignTeachers>
       <Educational></Educational>
-    </div>
+    </>
   )
 }
 
