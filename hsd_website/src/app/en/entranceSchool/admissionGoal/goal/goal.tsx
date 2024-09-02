cadf074dc5f32fd218e5e593935b7c1d42a10645
about-suntao <1550208137@qq.com>
2024-09-02 17:52:36 +0800

    fix 英文文案修改

--- hsd_website/src/app/en/entranceSchool/admissionGoal/goal/goal.tsx
+++ hsd_website/src/app/en/entranceSchool/admissionGoal/goal/goal.tsx
@@ -17,17 +17,17 @@ function Goal() {
               <Image src={goldImg} alt=''></Image>
             </div>
             <div className={styles.card_text}>
-              <p>U.S. high school enrollment upon enrollment</p>
-              <p>100% of students apply to the top 80 QS ranked universities in the world</p>
+              <p>Upon enrollment, students are registered with a U.S. high school.</p>
+              <p>100% of students are accepted into universities ranked in the top 80 globally according to QS rankings.</p>
             </div>
           </div>
           <div className={styles.list}>
-            <h3>AP Program</h3>
-            <p>80% of students enroll in top 50 colleges in the U.S.</p>
+            <h3>AP Track</h3>
+            <p>80% of students are admitted to the top 50 universities in the United States.</p>
           </div>
           <div className={styles.list}>
-            <h3>A-Level Program</h3>
-            <p>50% of students go on to the top 5 universities in the UK, top 3 in Hong Kong, and top 2 in Singapore.</p>
+            <h3>A-Level Track</h3>
+            <p>50% of students are admitted to the top 5 universities in the UK, the top 3 in Hong Kong, and the top 2 in Singapore.</p>
           </div>
         </div>
         <div className={styles.picture}>
